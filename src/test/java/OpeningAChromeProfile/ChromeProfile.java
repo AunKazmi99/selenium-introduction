f23464c49463e354b2f309bf1d6b2bdb35852c3b
AunKazmi99 <aunkazmi996772@gmail.com>
2023-09-03 10:40:46 -0700

    cleaned the code and removed the useless comments in profile code

--- src/test/java/OpeningAChromeProfile/ChromeProfile.java
+++ src/test/java/OpeningAChromeProfile/ChromeProfile.java
@@ -10,28 +10,18 @@ public class ChromeProfile {
 
     public static void main(String[] args) {
 
-        /*System.setProperty("webdriver.chrome.driver", "C:\\Users\\AunAbbas\\Desktop\\SeleniumUdemyResources" +
-                "\\SeleniumWebDriverDownload\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");*/
-        // Set the path to the profile directory of the new test profile
-        //String testProfilePath = "C:\\Users\\AunAbbas\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 1";
-
-        // Create ChromeOptions and set user data directory
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\AunAbbas\\Desktop" +
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AunAbbas\\Desktop" +
                 "\\SeleniumUdemyResources\\SeleniumWebDriverDownload" +
                 "\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--user-data-dir=C:\\Users\\AunAbbas\\AppData\\Local\\Google\\Chrome\\User Data\\");
         options.addArguments("--profile-directory=Profile 1");
 
-        // Initialize WebDriver with the custom profile
         ChromeDriver driver = new ChromeDriver(options);
+        
         driver.get("https://dev-online-ordering.innowi.com/branch/maak");
-
-        // Proceed with your automation tasks
-        // For example, navigate to the online ordering website and continue with your tasks
-
-        // Close the WebDriver
-        //driver.quit();
+        driver.quit();
     }
 }
 
